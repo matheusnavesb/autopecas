2b5c5fbd94a6445772ea9f2bbfc0b0ddce31a602
Matheus Naves <mnaves.dev@gmail.com>
2024-02-23 11:06:04 -0300

    parte 2 aula incompleto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { EstadoListComponent } from './components/estado/estado-list/estado-list
 import { EstadoFormComponent } from './components/estado/estado-form/estado-form.component';
 import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
 import { UsuarioFormComponent } from './components/usuario/usuario-form/usuario-form.component';
+import { estadoResolver } from './components/estado/resolver/estado-resolver';
 
 export const routes: Routes = [
     { path: 'estados', component: EstadoListComponent, title: 'Lista de Estados'},
     { path: 'estados/new', component: EstadoFormComponent, title: 'Novo Estado'},
+    { path: 'estados/edit/:id', component: EstadoFormComponent, resolve: {estado: estadoResolver}},
+
     { path: 'usuarios', component: UsuarioListComponent, title: 'Lista de Usuarios'},
     { path: 'usuarios/new', component: UsuarioFormComponent, title: 'Lista de Usuarios'}
 ];
--- src/app/components/estado/estado-form/estado-form.component.ts
+++ src/app/components/estado/estado-form/estado-form.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, ActivatedRouteSnapshot, RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -8,12 +9,13 @@ import { MatInputModule} from '@angular/material/input';
 import { EstadoService } from '../../../services/estado.service';
 import {MatCardModule} from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Estado } from '../../../models/estado.model';
 
 
 @Component({
   selector: 'app-estado-form',
   standalone: true,
-  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule],
+  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, RouterModule],
   templateUrl: './estado-form.component.html',
   styleUrl: './estado-form.component.css'
 })
@@ -23,34 +25,54 @@ export class EstadoFormComponent {
 
   constructor(private formBuilder: FormBuilder,
               private estadoService: EstadoService,
-              private router: Router){
+              private router: Router,
+              private activatedRoute: ActivatedRoute){
+
+
+      const estado: Estado = this.activatedRoute.snapshot.data['estado'];
+
       this.formGroup = formBuilder.group({
-        nome:['', Validators.required],
-        sigla:['', Validators.required]
+        id: [(estado && estado.id) ? estado.id : null],
+        nome:[(estado && estado.nome) ? estado.nome : '', Validators.required],
+        sigla:[(estado && estado.sigla) ? estado.sigla : '', Validators.required]
       });
     }
 
 
-    onSubmit(){
-      if(this.formGroup.valid){
-        const novoEstado = this.formGroup.value;
-        this.estadoService.insert(novoEstado).subscribe({
-          next: (estadoCadastrado) => {
-              this.router.navigateByUrl('/estados');
-          },
-          error: (err) => {
-            console.log('Erro ao salvar' + JSON.stringify(err));
-          },
-        });
-      }
-    }
-
     salvar(){
+        if(this.formGroup.valid){
+          if(this.formGroup.valid){
+            const estado = this.formGroup.value;
+            if(estado.id == null){
+              this.estadoService.insert(estado).subscribe({
+                next: (estadoCadastrado) => {
+                  this.router.navigateByUrl('/estados');
+                },
+
+              error: (err) => {
+                console.log('Erro ao incluir' + JSON.stringify(err));
+              }
+            });
+
+          }else{
+              this.estadoService.update(estado).subscribe({
+                next: (estadoAlterado) => {
+                  this.router.navigateByUrl('/estados');
+                },
+
+              error: (err) => {
+                console.log('Erro ao editar' + JSON.stringify(err));
+              }
+          });
+          }
+      }
 
     }
+
+  }
 
     excluir(){
 
-    }
-}
 
+    };
+}
