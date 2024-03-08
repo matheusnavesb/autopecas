3ba34ff36e902c39698579127dde6565bc29cb8c
Matheus Naves <mnaves.dev@gmail.com>
2024-03-08 10:57:50 -0300

    validacoes estado forms

--- src/app/components/estado/estado-form/estado-form.component.ts
+++ src/app/components/estado/estado-form/estado-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -32,12 +32,19 @@ export class EstadoFormComponent {
 
     this.formGroup = formBuilder.group({
       id: [(estado && estado.id) ? estado.id : null],
-      nome: [(estado && estado.nome) ? estado.nome : '', Validators.required],
-      sigla: [(estado && estado.sigla) ? estado.sigla : '', Validators.required]
+      nome: [(estado && estado.nome) ? estado.nome : '',
+      Validators.compose([Validators.required,
+      Validators.minLength(4)])],
+      sigla: [(estado && estado.sigla) ? estado.sigla : '',
+      Validators.compose([Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(2)])],
     });
   }
 
   salvar() {
+    // marca todos os campos do formulario como touched
+    this.formGroup.markAllAsTouched();
     if (this.formGroup.valid) {
       const estado = this.formGroup.value;
       if (estado.id == null) {
@@ -76,6 +83,32 @@ export class EstadoFormComponent {
         });
       }
     }
+  }
+
+  errorMessages: { [controlName: string]: { [errorName: string]: string } } = {
+    nome: {
+      required: 'O nome de ser informado.',
+      minlenght: 'O nome de possuir ao menos 4 caracteres.'
+    },
+    sigla: {
+      required: 'A sigla deve ser informada.',
+      minlenght: 'A sigla deve possuir 2 caracteres.',
+      maxlenght: 'A sigla deve possuir 2 caracteres.'
+    }
+  }
+
+  getErrorMessage(controlName: string, errors: ValidationErrors | null | undefined): string {
+    if (!errors) {
+      return '';
+    }
+    // retorna a mensagem de erro espesifica
+    for (const errorName in errors) {
+      if (errors.hasOwnProperty(errorName) && this.errorMessages[controlName][errorName]) {
+        return this.errorMessages[controlName][errorName];
+      }
+    }
+    // caso nao encontre o erro
+    return 'Erro não mapeado (entre em contato com o desenvolvedor)'
   }
 
 }
